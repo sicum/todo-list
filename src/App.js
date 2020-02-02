ae6ceade2c06cba4a4a1458e2a9a81a20a640309
Erick Gaillard <erickgaillard@Ericks-MacBook-Pro.local>
2020-02-02 15:58:23 -0800

    demo

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, Fragment } from 'react';
+import TodoList from './components/TodoList';
+import TodoItems from './components/TodoItems';
+import Navbar from './components/Navbar';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  inputElement = React.createRef()
+  constructor() {
+    super()
+    this.state = {
+      items: [],
+      currentItem: {
+        text: '',
+        key: '',
+      },
+    }
+  }
+  deleteItem = key => {
+    const filteredItems = this.state.items.filter(item => {
+      return item.key !== key
+    })
+    this.setState({
+      items: filteredItems,
+    })
+  }
+
+  handleInput = e => {
+    const itemText = e.target.value
+    const currentItem = { text: itemText, key: Date.now() }
+    this.setState({
+      currentItem,
+    })
+  }
+  addItem = e => {
+    e.preventDefault()
+    const newItem = this.state.currentItem
+    if (newItem.text !== '') {
+      const items = [...this.state.items, newItem]
+      this.setState({
+        items: items,
+        currentItem: { text: '', key: '' },
+      })
+    }
+  }
+  render() {
+    return (
+      <Fragment>
+        <Navbar />
+        <main className="inner cover">
+          <br/>
+          <TodoList
+            addItem={this.addItem}
+            inputElement={this.inputElement}
+            handleInput={this.handleInput}
+            currentItem={this.state.currentItem}
+          />
+          <TodoItems entries={this.state.items} deleteItem={this.deleteItem} />
+        </main>
+      </Fragment>
+    )
+  }
 }
 
-export default App;
+export default App
---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React, { Component, Fragment } from 'react'
+
+class Navbar extends Component {
+  render() {
+    return (
+			<Fragment>
+        <header className="masthead mb-auto">
+          <div className="inner">
+            <h3 className="masthead-brand">Todo Task</h3>
+          </div>
+        </header>
+			</Fragment>
+    )
+	}
+}
+
+export default Navbar
