93a81dda55a64aab218678d5b4a8a515a7bb7529
sicum <sicum.erick@gmail.com>
2020-02-02 17:13:21 -0800

    Updates

---
+++ precache-manifest.69562e7fed2acce863021c98af212117.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2441fea6ed2893f19d42bb372f0c3fb5",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "4be9aa69c6d7ae0a0630",
+    "url": "/todo-list/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "0bb4540eff12746e0b78",
+    "url": "/todo-list/static/js/2.9fcebf5e.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/todo-list/static/js/2.9fcebf5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4be9aa69c6d7ae0a0630",
+    "url": "/todo-list/static/js/main.87b9ba12.chunk.js"
+  },
+  {
+    "revision": "92f9e9f4ceb715dbe5b1",
+    "url": "/todo-list/static/js/runtime-main.f618dfff.js"
+  }
+]);
